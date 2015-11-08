658952b06305c74e5c922a5224370f2bef85adba
Cintia Del Rio Calvo <cintia.calvo@covata.com>
2015-11-08 21:03:16 +1100

    changing password to root

--- docker-config/run-openmrs-sdk.sh
+++ docker-config/run-openmrs-sdk.sh
@@ -23,8 +23,8 @@ mvn org.openmrs.maven.plugins:openmrs-sdk-maven-plugin:2.0.3:setup \
 -Dversion=2.3 \
 -DdbDriver=mysql \
 -DdbUri=jdbc:mysql://${DATABASE_HOST}/openmrs-${BUILDNUMBER} \
--DdbUser=test \
--DdbPassword=test
+-DdbUser=root \
+-DdbPassword=openmrs
 
 
 sleep 50000
