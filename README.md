e93002372e2c276753a71a792d44eae4f1556ce8
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2016-11-27 09:56:21 +1100

    ITSM-3956: something that runs demo data! at least you can login

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## Running it
+```
+# docker-compose up
+```
+
+## Other docker files and links
+- <https://wiki.openmrs.org/display/docs/Installing+OpenMRS+on+Docker>
+- <https://github.com/tusharsoni/OpenMRS-Docker/blob/master/Dockerfile>
+- <https://github.com/chaityabshah/openmrs-core-docker/blob/master/Dockerfile>
+- <https://github.com/bmamlin/openmrs-core-docker/blob/master/Dockerfile>
---
+++ openmrs-docker/setup-openmrs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+
+# docker 1.12 and healthcheck might help here
+echo "Going to sleep and hopefully get a database"
+sleep 15
+
+echo "Starting import of demo data. "
+mysql --user=${DB_USERNAME} --password=${DB_PASSWORD} --host=${DB_HOST} ${DB_DATABASE} < /tmp/demo-*.sql
+echo "Done... "
+
+echo "Writing configuration file:"
+cat > /usr/local/tomcat/openmrs-runtime.properties << EOF
+# Created from Docker setup script
+encryption.vector=OUbY/9CYYmUWwuC5m8ze6Q\=\=
+module.allow_web_admin=true
+connection.url=jdbc\:mysql\://${DB_HOST}\:3306/${DB_DATABASE}?autoReconnect\=true
+connection.username=${DB_USERNAME}
+auto_update_database=true
+encryption.key=NfHYNWEqiNpxRWceT/mCMA\=\=
+connection.password=${DB_PASSWORD}
+EOF
+
+cat /usr/local/tomcat/openmrs-runtime.properties
+
+echo "Running tomcat"
+catalina.sh "run"
