ce8c22a42e6da9b7c78bee74756cccbe775fee3d
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2016-11-27 21:21:06 +1100

    ITSM-3956: Capture demo data for refapp 2.5 mysql 5.5

--- README.md
+++ README.md
@@ -22,7 +22,16 @@ Application will be accessible on http://localhost:8080/openmrs.
 If you are running docker tools, change _localhost_ by the output of `docker-machine ip`.
 
 
-Use _CTRL + C_ to stop it all containers.
+Use _CTRL + C_ to stop it all containers. But make sure to destroy containers to delete any
+left overs volumes and data when doing changes to the docker configuration and images:
+```
+$ docker-compose down
+```
+
+## Demo data
+If you change _IMPORT_DB_DUMP_ in `docker-compose.yml` to false, database will remain empty,
+and wizard will show up.
+The current dump was take with `mysqldump` after running the wizard manually.
 
 
 ## Other similar docker images and relevant links
--- openmrs-docker/setup-openmrs.sh
+++ openmrs-docker/setup-openmrs.sh
@@ -1,28 +1,27 @@
 #!/bin/bash -eux
 
 # docker 1.12 and healthcheck might help here
-echo "Going to sleep and hopefully get a database"
+echo "Going to sleep. Hopefully we'll get a running database when waking up. "
 sleep 15
 
-#echo "Starting import of demo data. "
-#mysql --user=${DB_USERNAME} --password=${DB_PASSWORD} --host=${DB_HOST} ${DB_DATABASE} < /tmp/demo-*.sql
-#echo "Done... "
+if [ "${IMPORT_DB_DUMP}" == "true" ]; then
+  echo "Starting import of demo data. "
+  mysql --user=${DB_USERNAME} --password=${DB_PASSWORD} --host=${DB_HOST} ${DB_DATABASE} < /root/demo-refapp*.sql
+  echo "Done... "
+fi
 
-#echo "Writing configuration file:"
-#cat > /usr/local/tomcat/openmrs-runtime.properties << EOF
+echo "Writing configuration file:"
+cat > /root/.OpenMRS/openmrs-runtime.properties << EOF
 # Created from Docker setup script
-#encryption.vector=OUbY/9CYYmUWwuC5m8ze6Q\=\=
-#encryption.key=NfHYNWEqiNpxRWceT/mCMA\=\=
-#module.allow_web_admin=true
-#connection.url=jdbc\:mysql\://${DB_HOST}\:3306/${DB_DATABASE}?autoReconnect\=true
-#connection.username=${DB_USERNAME}
+#encryption.vector=X87Os8fUcYjR4jlQK09MXQ\=\=
+#encryption.key=oTNOfU2FLvN/7+7OjSggEg\=\=
+connection.url=jdbc\:mysql\://${DB_HOST}\:3306/${DB_DATABASE}?autoReconnect\=true&sessionVariables\=default_storage_engine\=InnoDB&useUnicode\=true&characterEncoding\=UTF-8
+module.allow_web_admin=true
+connection.username=${DB_USERNAME}
 #auto_update_database=true
-#connection.password=${DB_PASSWORD}
-#EOF
-#cat /usr/local/tomcat/openmrs-runtime.properties
-
-ls -lR /root/.OpenMRS/
-
+connection.password=${DB_PASSWORD}
+EOF
+cat /root/.OpenMRS/openmrs-runtime.properties
 
 echo "Running tomcat"
 catalina.sh "run"
